7c459c57d26a27d448f90002099a54a9913b8f87
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-02-11 23:52:54 +0000

    finished

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,16 @@ class AddressBook
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
       end
-    end
+    
+    
+     def remove_entry(name, phone_number, email)
+       index = 0
+       entries.each do |entry|
+         if name == entry.name && phone_number == entry.phone_number && email == entry.email
+             entries.delete_at(index)
+         
+         end
+         index += 1
+        end
+     end
+   end
