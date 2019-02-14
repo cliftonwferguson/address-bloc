0cbf409df8509168e6a88b3d62b7ea4c01379bed
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-02-14 23:58:32 +0000

    finished exercise

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 class AddressBook
     attr_reader :entries 
     def initialize
@@ -17,7 +18,14 @@ class AddressBook
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
       end
-    
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+      csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+    end
     
      def remove_entry(name, phone_number, email)
        index = 0
