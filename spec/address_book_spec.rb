ae6d473610f20eaa1a730cf2a237e64c10944bf7
cliftonwferguson <clifton.ferguson7@gmail.com>
2019-02-15 00:25:18 +0000

    first blocker

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -50,6 +50,13 @@ require_relative '../models/address_book'
        book_size = book.entries.size
        expect(book_size).to eq 5
      end
+     
+     describe "#import_from_csv" do
+      it "imports the correct number of entries" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+       expect(book_size).to eq 3
+     end
  
    end
     
